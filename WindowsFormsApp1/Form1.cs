17f68f282f75bdcfa8d2f077f0690f789dc46fa9
chikiryava <98350681+chikiryava@users.noreply.github.com>
2023-02-24 16:16:49 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+            if (student.Check(textBox2.Text) != -1 && student.Check(numericUpDown1.Value.ToString()) != -1 &&  student.CheckName(textBox1.Text)!="null")
+            {
+                student.height = double.Parse(numericUpDown1.Value.ToString());
+                double food = double.Parse(textBox2.Text);
+                student.name = textBox1.Text; 
+                student.SetEat(food);
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/Student.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal class Student
+    {
+        private double weight = 75;
+        public double height;
+        public string name;
+        public double GetEat()
+        { return weight; }
+        
+        public double Check(string variable)
+        {
+            if (variable == string.Empty)
+            {
+                MessageBox.Show("Поле не может быть пустым");
+                return -1;
+            }
+            if(!double.TryParse(variable, out double test))
+            {
+                MessageBox.Show("Вы ввели не число");
+                return -1;
+            }
+            else
+            {
+                double value = double.Parse(variable);
+                if (value <= 0) 
+                {
+                    MessageBox.Show("Значение не может быть меньше или равным 0");
+                    return -1;
+                } 
+                else
+                {
+                    return value;
+                }
+
+
+            }
+        }
+        
+        public string CheckName(string studentName)
+        {
+            bool testname = false;
+            if (studentName.Length == 0)  
+                testname = true;            
+            for(int i = 0; i < studentName.Length; i++)
+            {
+                if (!char.IsLetter(studentName[i]) || studentName[i] == ' ')
+                {
+                    testname = true;
+                }
+            }
+            if (testname == true)
+            {
+                MessageBox.Show("В имени могут содержаться только буквы и это поле не может быть пустым");
+                return null;
+            }
+            else
+                return name;
+        }
+        public void SetEat(double food)
+        {
+            if(food<5)
+                weight += food-1.8;
+            else if(food>5 && food < 10)
+            {
+                weight += (food - 1.8)*0.7;
+                height -= 1;
+            }
+            else
+            {
+                weight += (food - 1.8) * 0.5;
+                height -= 2;
+            }
+            MessageBox.Show($"Студент {name}\nРост {height}\nВес{weight}");
+                
+        } 
+
+    }
+}
